5618f8c8ed34bad6d38c22981018853c0da73d9d
Matteo.Verz <matteoalexandru2612@gmail.com>
2022-01-23 03:58:19 +0200

    Removed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_192504) do
-
-  create_table "contests", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
