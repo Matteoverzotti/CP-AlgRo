40ab41efd84d9e1dbe8ff363aa7404692e09677d
Matteo.Verz <matteoalexandru2612@gmail.com>
2022-03-01 10:47:40 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :lessons
+  resources :lessons do
+    #get '/lessons/:name', redirect { |path_params, req|
+    #"lessons/#{Lesson.where(name: path_params[:name]).first.id}"
+    #}
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   get 'home/about'
